e068078b1caf0338ad2e652dc069b776808f1065
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-10-15 14:06:56 +0100

    changes to kudi setup

--- src-server/server/SMSSender.js
+++ src-server/server/SMSSender.js
@@ -2,46 +2,56 @@
 import axios from 'axios';
 import FormData from 'form-data';
 
-export const KUDI_SMS_API = 'https://my.kudisms.net/api/corporate';
+
+export const KUDI_PROMOTIONAL_API = 'https://my.kudisms.net/api/sms';
 export const SENDER = 'DUV LIVE';
 
 export const convertToValidSMSPhoneNumber = (phone) => {
-  // remove all spaces
-  // remove +
+  // remove non-numeric characters
   let convertedPhoneNumber = phone.replace(/[^0-9]/g, '');
-  // convert 07 and 08 to 234
+
+  // handle local formats (07..., 08...) by converting to +234
   const phoneFirstTwoChars = convertedPhoneNumber.substring(0, 2);
   if (phoneFirstTwoChars === '07' || phoneFirstTwoChars === '08') {
-    convertedPhoneNumber = convertedPhoneNumber.replace(/^.{1}/g, '234');
+    convertedPhoneNumber = convertedPhoneNumber.replace(/^0/, '234');
   }
+
   // return valid number
   return convertedPhoneNumber.length === 13 ? convertedPhoneNumber : phone;
 };
 
-export const sendSMS = async ({ message, phone }) => {
+/**
+ * Send SMS using KudiSMS API
+ * @param {Object} params
+ * @param {string} params.message - Message to send
+ * @param {string} params.phone - Recipient phone number
+ * @param {boolean} [params.promotional=false] - If true, sends via promotional route
+ */
+export const sendSMS = async ({ message, phone}) => {
   const phoneNumber = convertToValidSMSPhoneNumber(phone);
 
   const data = new FormData();
-  data.append('token', process.env.KUDI_SMS_KEY); 
+  data.append('token', process.env.KUDI_SMS_KEY);
   data.append('senderID', SENDER);
   data.append('recipients', phoneNumber);
   data.append('message', message);
+  data.append('gateway', '2');
+  
+
+  const apiUrl = KUDI_PROMOTIONAL_API ;
 
   try {
-    const response = await axios.post(KUDI_SMS_API, data, {
+    const response = await axios.post(apiUrl, data, {
       headers: {
         ...data.getHeaders(),
       },
       maxBodyLength: Infinity,
     });
+
     console.log('SMS sent successfully:', response.data);
+    return response.data;
   } catch (error) {
     console.error('Error sending SMS:', error.response?.data || error.message);
+    throw error;
   }
 };
-
-
-
-
-// SMS CONTENT CAN BE FOUND HERE
-// https://docs.google.com/spreadsheets/d/1qV1qHePqdJbG5DI8-REKXZGvF4-YHuk3cU_vjjRtXMY/edit#gid=0
