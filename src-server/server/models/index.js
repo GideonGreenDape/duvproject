e46f16133c0278c41d0879aae309024001a7e7c1
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-10-17 19:14:07 +0100

    fix applied

--- src-server/server/models/index.js
+++ src-server/server/models/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import fs from 'fs';
-import path from 'path';
-import Sequelize from 'sequelize';
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname + '/../config/config.js'))[env];
