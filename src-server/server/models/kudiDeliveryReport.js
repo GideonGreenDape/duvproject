017d49252a0007ea1a1dc7e071ab12623e68ccb2
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-10-17 10:42:16 +0100

    changes made

--- src-server/server/models/kudiDeliveryReport.js
+++ src-server/server/models/kudiDeliveryReport.js
@@ -1,50 +1,19 @@
 'use strict';
-module.exports = function (sequelize, DataTypes) {
-  const DeliveryReport = sequelize.define(
-    'DeliveryReport',
-    {
-      status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      api_reference: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      code: {
-        type: DataTypes.STRING,
-      },
-      cost: {
-        type: DataTypes.STRING,
-      },
-      recipient: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      time: {
-        type: DataTypes.DATE,
-      },
-      network: {
-        type: DataTypes.STRING,
-      },
-      country: {
-        type: DataTypes.STRING,
-      },
-      description: {
-        type: DataTypes.STRING,
-      },
-      vendor: {
-        type: DataTypes.STRING,
-      },
-    },
-    {
-      classMethods: {
-        associate: function (models) {
-          // Example: if you have a Message or User model to relate it to
-          // DeliveryReport.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
-        },
-      },
-    }
-  );
+module.exports = (sequelize, DataTypes) => {
+  const DeliveryReport = sequelize.define('DeliveryReport', {
+    status: { type: DataTypes.STRING, allowNull: false },
+    api_reference: { type: DataTypes.STRING, allowNull: false },
+    code: { type: DataTypes.STRING },
+    cost: { type: DataTypes.STRING },
+    recipient: { type: DataTypes.STRING, allowNull: false },
+    time: { type: DataTypes.DATE },
+    network: { type: DataTypes.STRING },
+    country: { type: DataTypes.STRING },
+    description: { type: DataTypes.STRING },
+    vendor: { type: DataTypes.STRING },
+  }, {
+    tableName: 'deliveryreports',
+    timestamps: true,
+  });
   return DeliveryReport;
 };
